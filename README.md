22e8059950db95e830fbe6844d6de9ae7eeda0e8
Lucas <lmateus8673@gmail.com>
2022-01-09 10:53:44 -0300

    Adicionando novas tarefas ao README.md e Adicionando gitignore

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PotfolioGeral
 Junção dos Portifólios da equipe
+
+[x] Gerando servidor para implementação de download de documentos (Currículos);
+
+[] Iniciando layout do projeto.
+[] Definindo posições de Menu, Descrição, Perfil;
+[] Definindo cor padrão do projeto;
+[] Linkando todos os botões com os portfólios individuais.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = 8080;
 
 app.set('view engine', 'ejs');
 //Faz com que salve o arquivo em "uploads/" com o nome original do arquivo, data e extensão do arquivo
@@ -8,6 +9,8 @@ app.get("/", function(req, res){
     res.render("../views/index");
 });
 
-app.listen(8080, () => {
-    console.log("====> Servidor ligado!! <====")
+app.listen(PORT, () => {
+    console.log("\n>> Observação: Não utilize Live Server para verificar as atualizações do site estão sendo atualizadas.");
+    console.log(`>> Ao ligar o servidor no navegador, utilize: localhost:${PORT}`);
+    console.log(`\n====> Servidor ligado!! PORTA: ${PORT} <====\n`);
 });
