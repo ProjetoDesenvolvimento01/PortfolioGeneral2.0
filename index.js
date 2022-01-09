0c0cc2222158062d47a19afa1b2a7d939440022e
Lucas <lmateus8673@gmail.com>
2022-01-09 12:14:51 -0300

    Reparos de problemas com CSS e EJS

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+
+app.set('views', './views');
 app.set('view engine', 'ejs');
 //Faz com que salve o arquivo em "uploads/" com o nome original do arquivo, data e extensão do arquivo
 
 app.get("/", function(req, res){
-    res.render("../views/index");
+    res.render("index", { text: 'TESTE DE EJS'});
 });
 
 app.listen(PORT, () => {
